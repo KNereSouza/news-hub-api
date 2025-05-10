6c2f73c1ed3d6ab776fd976c4da4ef3fd30cc36a
KNereSouza <professional.neresk@gmail.com>
2025-05-09 23:26:06 -0300

    feat: create User model

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+import { DataTypes, UUIDV4 } from "sequelize";
+import { sequelize } from "../config/database";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "editor", "author"),
+      defaultValue: "author",
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "Users",
+    timestamps: true,
+  }
+);
