a72001520625069e2bf6ede40ab4ef08dce28552
KNereSouza <professional.neresk@gmail.com>
2025-05-13 23:00:42 -0300

    feat: improve error handling in AuthMiddleware

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -4,13 +4,13 @@ const secretKey = process.env.JWT_SECRET;
 
 const authenticateToken = (request, response, next) => {
   if (!secretKey) {
-    console.error("JWT_SECRET is not defined in the environment variables.");
     return response
       .status(500)
       .json({ message: "Internal server error. Please try again later." });
   }
 
   const authHeader = request.headers.authorization;
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return response.status(401).json({
       message: "Access denied, token not provided or invalid format.",
@@ -19,19 +19,16 @@ const authenticateToken = (request, response, next) => {
 
   const token = request.headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    return response
-      .status(401)
-      .json({ message: "Access denied, token not provided." });
-  }
-
   try {
     const decoded = jwt.verify(token, secretKey);
     request.user = decoded;
     next();
   } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return response.status(403).json({ message: "Token has expired." });
+    }
     return response.status(403).json({ message: "Invalid or expired token." });
   }
 };
 
-export { authenticateToken };
+export default authenticateToken;
