e955ca7924f299a1a1c59178218ed7241f1a48ea
KNereSouza <professional.neresk@gmail.com>
2025-06-07 23:46:58 -0300

    feat: create GetUsersService class

---
+++ src/services/GetUsersService.js
@@ -0,0 +1,18 @@
+import UsersRepository from "../repositories/UsersRepository.js";
+
+export default class GetUsersService {
+  constructor(usersRepository = new UsersRepository()) {
+    this.usersRepository = usersRepository;
+  }
+
+  async handle() {
+    try {
+      const data = await this.usersRepository.getUsers();
+      const users = data.map((object) => object.dataValues);
+      return users;
+    } catch (error) {
+      console.error("Error getting users:", error);
+      throw new Error(`Unable to get users: ${error.message}`);
+    }
+  }
+}
