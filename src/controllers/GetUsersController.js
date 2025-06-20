09289bde458c9e4873ef92733232ba4f0857c212
KNereSouza <professional.neresk@gmail.com>
2025-06-20 16:04:26 -0300

    feat: update GetUsers controller to deal with single user query

--- src/controllers/GetUsersController.js
+++ src/controllers/GetUsersController.js
@@ -6,9 +6,24 @@ export default class GetUsersController {
   }
 
   async handle(request, response) {
+    const userId = request.params.id;
+
     try {
-      const users = await this.getUsersService.handle();
-      return response.status(200).json(users);
+      if (!userId) {
+        const users = await this.getUsersService.handle();
+        return response.status(200).json(users);
+      }
+
+      const user = await this.getUsersService.handle(userId);
+      console.log(user);
+
+      if (!user) {
+        return response.status(404).json({
+          message: `User with ID ${userId} not found. Please verify the user ID and try again.`,
+        });
+      }
+
+      return response.status(200).json(user);
     } catch (error) {
       return response
         .status(500)
