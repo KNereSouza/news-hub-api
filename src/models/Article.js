2292f93d2cc54a9739e82a76879c18f1b27a3d55
KNereSouza <professional.neresk@gmail.com>
2025-06-23 21:21:46 -0300

    feat: create Article sequelize model

---
+++ src/models/Article.js
@@ -0,0 +1,64 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+import generateSlug from "../utils/generateSlug.js";
+
+const Article = sequelize.define(
+  "Article",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    thumbnailUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    status: {
+      type: DataTypes.ENUM("draft", "published", "archived"),
+      defaultValue: "draft",
+    },
+    publishedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Article",
+    tableName: "Articles",
+    timestamps: true,
+  }
+);
+
+Article.beforeValidate((article, options) => {
+  const slug = generateSlug(article.title);
+  article.slug = slug;
+});
+
+Article.beforeSave((article) => {
+  if (article.status === "published" && !article.publishedAt) {
+    article.publishedAt = new Date();
+  }
+});
+
+export default Article;
