572da188336da459b1ec11e37a053ed703c048e4
KNereSouza <professional.neresk@gmail.com>
2025-06-26 17:03:01 -0300

    feat: create PATCH route /articles/:id

--- src/routes/articles.routes.js
+++ src/routes/articles.routes.js
@@ -5,10 +5,12 @@ import { Router } from "express";
 import validateArticleData from "../middlewares/ValidateArticleDataMiddleware.js";
 import authenticateToken from "../middlewares/AuthMiddleware.js";
 import checkRoles from "../middlewares/CheckRolesMiddleware.js";
+import isOwner from "../middlewares/IsOwnerMiddleware.js";
 
 // Controllers
 import CreateArticleController from "../controllers/CreateArticleController.js";
 import GetArticlesController from "../controllers/GetArticlesController.js";
+import UpdateArticlesController from "../controllers/UpdateArticleController.js";
 
 const articlesRouter = Router();
 
@@ -29,5 +31,14 @@ articlesRouter.get("/:slug", async (request, response) => {
 articlesRouter.get("/", async (request, response) => {
   await new GetArticlesController().handle(request, response);
 });
+
+articlesRouter.patch(
+  "/:id",
+  authenticateToken,
+  isOwner(["admin", "editor"]),
+  async (request, response) => {
+    await new UpdateArticlesController().handle(request, response);
+  }
+);
 
 export default articlesRouter;
