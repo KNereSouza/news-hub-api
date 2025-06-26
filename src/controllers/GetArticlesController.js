d658d96fc3a8add52a6dead93fec8a76ab705349
KNereSouza <professional.neresk@gmail.com>
2025-06-26 10:31:05 -0300

    feat: create GetArticles controller

---
+++ src/controllers/GetArticlesController.js
@@ -0,0 +1,39 @@
+import GetArticlesService from "../services/GetArticlesService.js";
+
+export default class GetArticlesController {
+  constructor(getArticlesService = new GetArticlesService()) {
+    this.getArticlesService = getArticlesService;
+  }
+
+  async handle(request, response) {
+    const queryData = {};
+
+    if (typeof request.params.slug === "string") {
+      queryData.slug = request.params.slug;
+    }
+
+    if (typeof request.query.authorId === "string") {
+      queryData.authorId = request.query.authorId;
+    }
+
+    if (typeof request.query.categorySlug === "string") {
+      queryData.categorySlug = request.query.categorySlug;
+    }
+
+    try {
+      const articles = await this.getArticlesService.handle(queryData);
+
+      if (articles.length === 0) {
+        return response.status(404).json({
+          message: "No articles found matching the provided criteria.",
+        });
+      }
+
+      return response.status(200).json(articles);
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ message: "Internal server error. Try again later" });
+    }
+  }
+}
