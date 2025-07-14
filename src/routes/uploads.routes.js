a42af310370e416cbad3055a13850623fb49dbd2
KNereSouza <professional.neresk@gmail.com>
2025-07-14 17:55:29 -0300

    feat: create POST uploads/video route

--- src/routes/uploads.routes.js
+++ src/routes/uploads.routes.js
@@ -6,6 +6,7 @@ import validateFileType from "../middlewares/ValidateFileTypeMiddleware.js";
 import checkRoles from "../middlewares/CheckRolesMiddleware.js";
 
 import UploadImageController from "../controllers/UploadImageController.js";
+import UploadVideoController from "../controllers/UploadVideoController.js";
 
 const uploadsRouter = Router();
 
@@ -17,6 +18,17 @@ uploadsRouter.post(
   validateFileType,
   async (request, response) => {
     await new UploadImageController().handle(request, response);
+  }
+);
+
+uploadsRouter.post(
+  "/video",
+  authenticateToken,
+  checkRoles(["admin", "author"]),
+  upload.single("file"),
+  validateFileType,
+  async (request, response) => {
+    await new UploadVideoController().handle(request, response);
   }
 );
 
